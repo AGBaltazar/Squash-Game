38e4e2d3617af6241fd332d604999f70640a4640
AGBaltazar <agee.baltazar@gmail.com>
2024-11-01 13:17:08 -0700

    Updated mechanics between player and ball

--- src/game/scenes/GameMechanics.js
+++ src/game/scenes/GameMechanics.js
@@ -10,6 +10,10 @@ export class GameMechanics extends Scene {
         this.courtData = data.courtData;
     }
 
+    preload() {
+        this.load.image('ball', 'assets/Ball.png');
+    }
+
     create() {
         this.physics.world.setBoundsCollision(true, true, true, false);
 
@@ -22,56 +26,78 @@ export class GameMechanics extends Scene {
         const banditX = canvasWidth * 0.2;
         const stripeX = canvasWidth * 0.8;
         const playerY = canvasHeight * 0.5;
-        
+
         this.bandit = this.physics.add.sprite(banditX, playerY, 'bandit').setScale(0.5).setCollideWorldBounds(true).setImmovable(true);
         this.stripe = this.physics.add.sprite(stripeX, playerY, 'stripe').setScale(0.5).setCollideWorldBounds(true);
 
-        this.ball = this.physics.add.group({key: 'ball', frame: 3, frameQuantity: 1, bounceX: 1, bounceY: 1, collideWorldBounds:true, velocityX: 100, velocityY: 100});
+        this.ball = this.physics.add.image(400, 500, 'ball').setCollideWorldBounds(true).setBounce(1);
+        this.ball.setData('onBandit', true);
 
-        this.physics.add.collider(this.ball, this.bandit)
+        this.physics.add.collider(this.ball, this.bandit);
 
         EventBus.emit('current-scene-ready', this);
+
+        this.isSwinging = false; 
     }
 
     update() {
         let cursors = this.input.keyboard.createCursorKeys();
-
-        this.bandit.setVelocity(0)
+        this.bandit.setVelocity(0);
 
         if (cursors.left.isDown) {
             this.bandit.setVelocityX(-200);
             this.bandit.anims.play('left', true);
-        } 
-        else if (cursors.right.isDown) {
+        } else if (cursors.right.isDown) {
             this.bandit.setVelocityX(200);
             this.bandit.anims.play('right', true);
-        } 
-        
-        if (cursors.up.isDown ) {
+        }
+
+        if (cursors.up.isDown) {
             this.bandit.setVelocityY(-200);
-            this.bandit.anims.play('up', true)
-        } 
-        else if (cursors.down.isDown ) {
-            this.bandit.setVelocityY(200); 
+            this.bandit.anims.play('up', true);
+        } else if (cursors.down.isDown) {
+            this.bandit.setVelocityY(200);
             this.bandit.anims.play('down', true);
         }
 
-        //if(cursors.space.isDown ){}
+        if (cursors.space.isDown && !this.isSwinging) {
+            this.bandit.anims.play('swing', true);
+            this.isSwinging = true;
 
-        //If the ball goes out of bounds, then reset the match
-        if(this.ball.y > 100){
+        } else if (cursors.space.isUp) {
+            this.isSwinging = false;
+        }
+
+        if (this.ball.y > this.scale.height) {
             this.scoredReset();
         }
     }
 
-    scoredReset(){
+    hitBall(ball, bandit) {
+        let difference = ball.x - bandit.x;
+        
+        if (difference < 0) {
+            ball.setVelocityX(200);
+            ball.setVelocityY(-100); 
+        } else if (difference > 0) {
+            ball.setVelocityX(-200);
+            ball.setVelocityY(-100);
+        } else {
+            ball.setVelocityY(-200);
+        }
+        
+        console.log('Hit registered');
+    }
+
+    scoredReset() {
         this.ball.setVelocity(0);
         this.ball.setPosition(this.bandit.x, 500);
-        console.log('score')
+        this.bandit.setPosition(this.bandit.x, 500)
+        this.ball.setData('onBandit', true);
+        console.log('Score reset');
     }
 
     changeScene() {
-
         this.scene.start('MainMenu');
     }
 }
